be36e33aeef21a27a82148125ee471682291902e
ideaworld <gbwlovehp@gmail.com>
2016-08-17 01:25:45 -0400

    basic token defined

--- MonkeyLex.py
+++ MonkeyLex.py
@@ -12,3 +12,17 @@ tokens = keywords + (
 
 t_ignore = '\t'
 
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    if t.value in keywords:
+        t.type = t.value
+    return t;
+
+def t_NUMBER(t):
+    r"""(\d+(\.\d*)?|\.\d+)([eE][-+]? \d+)?"""
+    t.value = float(t.value)
+    return t
+
+t_STRING = r'\".*?\"'
+t_BOOL = r'(true)|(false)'
+t_EQUAL = r'='
